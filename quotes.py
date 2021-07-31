5e41660d3f82073fc74cc823a8df13fdb753f287
Saran Mahadev <sarandevnet@gmail.com>
2021-07-31 22:35:05 +0530

    Added Quotes Script

---
+++ quotes.py
@@ -0,0 +1,14 @@
+import requests
+from plyer import notification
+def get_quote():
+    """ Gets Quote From the Site """
+    url = "https://zenquotes.io/api/random"
+    resp = requests.get(url)
+    quote = resp.json()[0]["q"]
+    author = resp.json()[0]["a"]
+    return quote,author
+def send_quote():
+    """ Sends Quote to the Desktop"""
+    quote = get_quote()   
+    notification.notify(title = quote[1] , message = quote[0], app_name = "Quote of the Day" )
+send_quote()
