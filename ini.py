a1c2e288bfc342e94e7a52ea03b0e6aadb801a14
Saran Mahadev <sarandevnet@gmail.com>
2021-07-31 22:57:02 +0530

    Added Github Initializer

---
+++ ini.py
@@ -0,0 +1,11 @@
+from os import getcwd
+import subprocess
+import os
+
+subprocess.run(["git","init"])
+with open(os.getcwd() + "\README.md","w+") as file:
+    file.write("### Title"+"\n"+"---")    
+subprocess.run(["git","add","."])
+subprocess.run(["git","commit","-m","Initial commit"])
+subprocess.run(["git","remote","add","origin",input("Give Your GitHub Link")])
+subprocess.run(["git","push","-u","origin","master"])
